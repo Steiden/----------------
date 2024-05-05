ca8f5540e7b32325f2986ef0eb04fe9d8156655c
Daniil <baldindaniilza@mail.ru>
2024-05-05 18:05:10 +0500

    asdfa

--- client/src/api/config.tsx
+++ client/src/api/config.tsx
@@ -3,14 +3,4 @@ export const BASE_URL = 'https://ya-praktikum.tech/api/v2';
 export const endpoints: Record<string, string> = {
     'login': `${BASE_URL}/auth/signin`,
     'register': `${BASE_URL}/auth/signup`,
-    'logout': `${BASE_URL}/auth/logout`,
-    'profile': `${BASE_URL}/auth/user`,
-    'forgot': `${BASE_URL}/auth/forgot`,
-    'reset': `${BASE_URL}/auth/reset`,
-    'users': `${BASE_URL}/user/search`,
-    'user': `${BASE_URL}/user`,
-    'chats': `${BASE_URL}/chats`,
-    'chat': `${BASE_URL}/chats/`,
-    'messages': `${BASE_URL}/chats/`,
-    'message': `${BASE_URL}/chats/`,
 }
